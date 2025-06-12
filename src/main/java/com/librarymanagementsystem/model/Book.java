fa9d06d29dce9753e348c300391009c7babc6e03
erkut <kciftlikci@gmail.com>
2025-06-12 12:29:46 +0300

    feat: Implement Book and Category model classes

--- src/main/java/com/librarymanagementsystem/model/Book.java
+++ src/main/java/com/librarymanagementsystem/model/Book.java
@@ -1,4 +1,84 @@
 package com.librarymanagementsystem.model;
 
+/**
+ * The class which represents the book object class
+ * This class inculudes an ID, a Title, Category and Published Year for each book.
+ */
 public class Book {
+    private String id;        // Book ID
+    private String title;     // Book Title
+    private String author;    // Book Author
+    private int category;     // Book Category ID (Kategori ID'si)
+    private int year;         // Book Year
+
+    /**
+     * The constructor of book class.
+     * Initializes all attributes.
+     *
+     * @param id Unique ID for book.
+     * @param title The tittle of book.
+     * @param author The author of book.
+     * @param category The categoryID which book belongs it.
+     * @param year Publissh year of book.
+     */
+    public Book(String id, String title, String author, int category, int year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.year = year;
+    }
+
+    // Getter Methods
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    // Setter Methods (İsteğe bağlı, ancak güncelleme işlemleri için faydalı olabilir)
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", category=" + category +
+                ", year=" + year +
+                '}';
+    }
 }
--- src/main/java/com/librarymanagementsystem/model/Category.java
+++ src/main/java/com/librarymanagementsystem/model/Category.java
@@ -1,4 +1,58 @@
 package com.librarymanagementsystem.model;
 
+/**
+ * The class which represents the book category object class.
+ * This class inculudes category ID, Category name for each category.
+ */
 public class Category {
+    private int id;       // Category ID
+    private String name;  // Category name
+
+    /**
+     * Constructor of Category class .
+     * Initializes all attributes.
+     *
+     * @param id Unique ID for book.
+     * @param name name of Category.
+     */
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * Category class constructor without ID
+     * Used for cases where the ID will be assigned automatically when adding a new category
+     *
+     * @param name The name of category
+     */
+    public Category(String name) {
+        this.name = name;
+    }
+
+    // Getter methods
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Setter methods
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
